b2e6ec8c8ba5a546178ba7bc2762f920d28fc61c
quangtung26 <quangtung2681@gmail.com>
2024-05-20 17:30:44 +0700

    update requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+def get_requirements(path: str):
+    return [l.strip() for l in open(path)]
+
 setup(
     name='Todo maker',
     version='1.0.0',
@@ -8,6 +11,7 @@ setup(
     keywords='Todo',
     python_requires='>=3.10',
     packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
     entry_points={
         'console_scripts': [
             'todo=main:main'
